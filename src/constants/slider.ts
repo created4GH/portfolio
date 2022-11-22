3701e63c65287e43b82d6e8c2118034021b712dc
created4gitHub <koretskyi10@gmail.com>
2022-11-23 00:10:36 +0200

    updated main

--- src/constants/slider.ts
+++ src/constants/slider.ts
@@ -2,32 +2,32 @@ import { SlideInfoType } from "../interfaces/common";
 
 const MARKUP_IMG_PATH = './img/slides/markup/';
 const PET_PROJECTS_IMG_PATH = './img/slides/pet-projects/';
-
+const DEF_GH_URL = 'https://created4github.github.io/';
 
 export const MarkupSlides: SlideInfoType[] = [
     {
         title: 'Relvise',
         techStack: ['HTML, SCSS'],
         imgPath: MARKUP_IMG_PATH + 'relvise.jpg',
-        url: ''
+        url: DEF_GH_URL + 'relvise'
     },
     {
         title: 'Gocorna',
         techStack: ['HTML', 'SCSS'],
         imgPath: MARKUP_IMG_PATH + 'gocorna.jpg',
-        url: ''
+        url: DEF_GH_URL + 'gocorna'
     },
     {
         title: 'Real-Estate',
         techStack: ['HTML', 'SCSS', 'JS'],
         imgPath: MARKUP_IMG_PATH + 'real-estate.jpg',
-        url: ''
+        url: DEF_GH_URL + 'real-estate'
     },
     {
         title: 'Rhythm',
         techStack: ['HTML', 'SCSS', 'JS'],
         imgPath: MARKUP_IMG_PATH + 'rhythm.jpg',
-        url: ''
+        url: DEF_GH_URL + 'rhythm'
     },
 ]
 
@@ -36,24 +36,24 @@ export const PetProjectsSlides: SlideInfoType[] = [
         title: 'My Website',
         techStack: ['React, SCSS'],
         imgPath: PET_PROJECTS_IMG_PATH + 'my-website.jpg',
-        url: ''
+        url: DEF_GH_URL + 'my-website'
     },
     {
         title: 'Taxi-park',
         techStack: ['React Redux', 'SCSS'],
         imgPath: PET_PROJECTS_IMG_PATH + 'taxi-park.jpg',
-        url: ''
+        url: DEF_GH_URL + 'taxi-park'
     },
     {
         title: 'Users',
         techStack: ['React Redux-Saga', 'SCSS'],
         imgPath: PET_PROJECTS_IMG_PATH + 'users.jpg',
-        url: ''
+        url: DEF_GH_URL + 'users'
     },
     {
         title: 'Note-Editor',
         techStack: ['MERN', 'SCSS'],
         imgPath: PET_PROJECTS_IMG_PATH + 'note-editor.jpg',
-        url: ''
+        url: DEF_GH_URL + 'note-editor'
     },
 ]
